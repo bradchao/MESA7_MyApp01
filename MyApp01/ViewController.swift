3ae87f3f458c0ed2b5ed408208acb23dc654f6c3
iii <iii@userteki-MacBook.local>
2017-06-19 14:57:32 +0800

    v1

--- MyApp01/ViewController.swift
+++ MyApp01/ViewController.swift
@@ -10,14 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnClick: UIButton!
+    @IBOutlet weak var labelMesg: UILabel!
+    
+    @IBAction func sayYa(_ sender: Any) {
+        labelMesg.text = "Ya!"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
